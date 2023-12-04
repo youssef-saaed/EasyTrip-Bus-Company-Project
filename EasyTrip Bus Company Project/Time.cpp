98673358765a941124fd7c568267eeb2878e44d9
Habiba Arafa <145590935+Habiba-Arafa@users.noreply.github.com>
2023-12-04 21:28:49 +0200

    Update Time.cpp

--- EasyTrip Bus Company Project/Time.cpp
+++ EasyTrip Bus Company Project/Time.cpp
@@ -3,59 +3,47 @@
 #include <iostream>
 using namespace std;
 
-class Time {
-private:
-    int hour;
-    int minute;
-    int second;
+// Constructor
+Time::Time(int h, int m, int s) {
+    hour = h;
+    minute = m;
+    second = s;
+}
 
-public:
-    // Constructor
-    Time(int h = 0, int m = 0, int s = 0) {
-        hour = h;
-        minute = m,
-            second = s;
-    }
+// Overload + operator for adding minutes
+Time Time::operator+(int minutes) const {
+    Time outcome(*this);
+    outcome.minute += minutes;
+    outcome.handlingTime();
+    return outcome;
+}
 
-    // Overload + operator for adding minutes
-    Time operator+(int minutes) const {
-        Time outcome(*this);
-        outcome.minute += minutes;
-        outcome.handlingTime();
-        return outcome;
-    }
+void Time::addSeconds(int seconds) {
+    second += seconds;
+    handlingTime();
+}
 
-    void addSeconds(int seconds) {
-        second += seconds;
-        handlingTime();
-    }
+// Function to handle time (hours, minutes, and seconds)
+void Time::handlingTime() {
+    int additionalMinutes = second / 60;
+    second %= 60;
+    minute += additionalMinutes;
 
-    // Function to handle time ( hours, minutes, and seconds)
-    void handlingTime() {
-        int additionalMinutes = second / 60;
-        second %= 60;
-        minute += additionalMinutes;
+    int additionalHours = minute / 60;
+    minute %= 60;
+    hour += additionalHours;
+}
 
-        int additionalHours = minute / 60;
-        minute %= 60;
-        hour += additionalHours;
+// Function to check if the current time is working hours
+bool Time::isWorkingHours() const {
+    return (hour >= 4 && hour < 10);
+}
 
-    }
-
-    // Function to check if the current time is working hours
-
-    bool isWorkingHours() const {
-        return (hour >= 4 && hour < 10);
-    }
-
-    // Function to display the time
-    void display() const {
-        cout << hour << ":" << minute << ":" << second;
-
-
-    }
-};
-
+// Function to display the time
+string Time::display() const {
+    string time = to_string(hour) + ":" + std::to_string(minute);
+    return time;
+}
 
 
 
