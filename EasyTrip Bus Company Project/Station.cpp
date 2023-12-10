42e027118efbcb7d54a079283bb65ac2f14b61e9
Habiba Arafa <145590935+Habiba-Arafa@users.noreply.github.com>
2023-12-10 22:17:44 +0200

    Update Station.cpp

--- EasyTrip Bus Company Project/Station.cpp
+++ EasyTrip Bus Company Project/Station.cpp
@@ -4,12 +4,14 @@
 Station::Station(int number, int numberOfBusesAvailable, int numberOfMovedBuses) : stationNumber(number) , numberOfBusesAvailable(numberOfBusesAvailable), numberOfMovedBuses(numberOfMovedBuses), busesInStation(numberOfBusesAvailable) , movedBuses(numberOfMovedBuses) {};
 
 void Station::addBusToStation(Bus* bus) {
+    numberOfBusesAvailable++ ;
     busesInStation.enqueue(bus);
 }
 
 void Station::removeBusFromStation() {
     if (!busesInStation.isEmpty()) {
         Node<Bus*>* busNode = busesInStation.dequeue();
+        numberOfBusesAvailable--;
         movedBuses.push(busNode->data);
         delete busNode;
     }
@@ -21,4 +23,20 @@ arrayList<Bus*> Station::getMovedBuses() const {
 
 Queue<Bus*> Station::getBusesInStation() const {
     return busesInStation;
+}
+
+void Station::SetNumberOfAvailableBuses(int numberOfBusesAvailable) {
+    this->numberOfBusesAvailable = numberOfBusesAvailable;
+}
+
+int Station::GetNumberOfAvailableBuses() {
+    return numberOfBusesAvailable;
+}
+
+void Station::SetNumberOfMovedBuses(int numberOfMovedBuses) {
+    this->numberOfMovedBuses = numberOfMovedBuses;
+}
+
+int Station::GetNumberOfMovedBuses() {
+    return numberOfMovedBuses;
 }
