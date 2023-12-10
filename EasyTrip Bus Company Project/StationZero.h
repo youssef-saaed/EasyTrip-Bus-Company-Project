6bceea24a29b6e1699f145334f3f9b01819bb91d
Habiba Arafa <145590935+Habiba-Arafa@users.noreply.github.com>
2023-12-10 22:40:18 +0200

    Update StationZero.h

--- EasyTrip Bus Company Project/StationZero.h
+++ EasyTrip Bus Company Project/StationZero.h
@@ -5,11 +5,5 @@ class StationZero : public Station {
 public:
     StationZero(int number, int numberOfBusesAvailable, int numberOfMovedBuses);
 
-    void addBusToStation(Bus* bus) override;
-
-    void removeBusFromStation() override;
-
-    arrayList<Bus*> getMovedBuses() const override;
-
-    Queue<Bus*> getBusesInStation() const override;
+   
 };
