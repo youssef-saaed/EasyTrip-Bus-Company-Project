faea80932416460eda6a66775773865809a56f91
Habiba Arafa <145590935+Habiba-Arafa@users.noreply.github.com>
2023-12-10 22:55:02 +0200

    Update Stations.cpp

--- EasyTrip Bus Company Project/Stations.cpp
+++ EasyTrip Bus Company Project/Stations.cpp
@@ -17,9 +17,44 @@ arrayList<Bus*> Stations::getMovedBuses() const {
 }
 
 void Stations::addPassengerToStation(Passenger* passenger) {
+    NumOfWaitingPassengers++;
     waitingPassengers.Enqueue(passenger, passenger->getPriority());
 }
 
 void Stations::addRecentBus(Bus* bus) {
+    numOfRecentBuses++ ;
     recentBuses.enqueue(bus);
+}
+
+void Stations::SetNumOfAvailableForwardBuses(int numOfAvailableForwardBuses) {
+    NumOfAvailableForwardBuses = numOfAvailableForwardBuses;
+}
+
+void Stations::SetNumOfAvailableBackwardBuses(int numOfAvailableBackwardBuses) {
+    NumOfAvailableBackwardBuses = numOfAvailableBackwardBuses;
+}
+
+void Stations::SetNumOfRecentBuses(int numOfRecentBuses) {
+    NumOfRecentBuses = numOfRecentBuses;
+}
+
+void Stations::SetNumOfWaitingPassengers(int numOfWaitingPassengers) {
+    NumOfWaitingPassengers = numOfWaitingPassengers;
+}
+
+
+int Stations::GetNumOfAvailableForwardBuses() const {
+    return NumOfAvailableForwardBuses;
+}
+
+int Stations::GetNumOfAvailableBackwardBuses() const {
+    return NumOfAvailableBackwardBuses;
+}
+
+int Stations::GetNumOfRecentBuses() const {
+    return NumOfRecentBuses;
+}
+
+int Stations::GetNumOfWaitingPassengers() const {
+    return NumOfWaitingPassengers;
 }
