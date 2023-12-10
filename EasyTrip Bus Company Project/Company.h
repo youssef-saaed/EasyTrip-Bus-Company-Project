9a15e3ef91e8b18a780f82db9fa54aa9d4aed63d
youssef-saaed <youssefsaid2015@hotmail.com>
2023-12-10 19:23:04 +0200

    .

--- EasyTrip Bus Company Project/Company.h
+++ EasyTrip Bus Company Project/Company.h
@@ -5,6 +5,8 @@
 #include "Passenger.h"
 #include "arrayList.h"
 #include "Station.h"
+#include "PriorityQueue.h"
+#include "Event.h"
 
 class Company
 {
@@ -13,5 +15,14 @@ private:
 	std::ofstream* OutputFileHandler;
 	Queue<Passenger*> FinishedPassengers;
 	arrayList<Station*> Stations;
+	PriorityQueue<Event*> Events;
+	int NumberOfStation, NumberOfWBuses, NumberOfMBuses, TimeBetweenEachStation, WBusCapacity, MBusCapacity, NumOfTripsBeforeCheckup, WBusCheckupDur, MBusCheckupDur, MaxNormalPassengerWaiting, GetOnOffDur;
+	void ReadInputFile();
+	void ProduceOutputFile();
+public:
+	Company(std::string InputDirectory = "input.txt", std::string OutputDirectory = "output.txt");
+	void Simulate();
+	~Company();
+	
 };
 
