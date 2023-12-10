36fcc30fde08029dfca0e4ab005ad50f59ef4266
Habiba Arafa <145590935+Habiba-Arafa@users.noreply.github.com>
2023-12-10 22:41:04 +0200

    Update StationZero.cpp

--- EasyTrip Bus Company Project/StationZero.cpp
+++ EasyTrip Bus Company Project/StationZero.cpp
@@ -3,22 +3,3 @@
 
 StationZero::StationZero(int number, int numberOfBusesAvailable, int numberOfMovedBuses) : Station(number, numberOfBusesAvailable, numberOfMovedBuses) {};
 
-void StationZero::addBusToStation(Bus* bus) {
-    this->getBusesInStation().enqueue(bus);
-}
-
-void StationZero::removeBusFromStation() {
-    if (!this->getBusesInStation().isEmpty()) {
-        Node<Bus*>* busNode = this->getBusesInStation().dequeue();
-        this->getMovedBuses().push(busNode->data);
-        delete busNode;
-    }
-}
-
-arrayList<Bus*> StationZero::getMovedBuses() const {
-    return this->getMovedBuses();
-}
-
-Queue<Bus*> StationZero::getBusesInStation() const {
-    return this->getBusesInStation();
-}
