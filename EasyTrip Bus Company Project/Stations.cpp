8188f99c74e06be579b6167bf8e497dc9c3879ba
Habiba Arafa <145590935+Habiba-Arafa@users.noreply.github.com>
2024-01-03 22:07:20 +0200

    Update Stations.cpp

--- EasyTrip Bus Company Project/Stations.cpp
+++ EasyTrip Bus Company Project/Stations.cpp
@@ -1,6 +1,9 @@
 #include "Station.h"
 #include "Stations.h"
-
+#include <typeinfo>
+#include <MBus.h>
+#include <WBus.h>
+#include<arrayList.h>
 Stations::Stations(int number, int maxNumberOfBuses, int maxNumberOfPassengers) : Station(number, maxNumberOfBuses), availableForwardBuses(maxNumberOfBuses), availableBackwardBuses(maxNumberOfBuses), recentBuses(maxNumberOfBuses), forwardNP(maxNumberOfPassengers), backwardNP(maxNumberOfPassengers), forwardWP(maxNumberOfPassengers), backwardWP(maxNumberOfPassengers),forwardSP(maxNumberOfPassengers), backwardSP(maxNumberOfPassengers)
 {
 };
@@ -12,6 +15,166 @@ Queue<Bus*> Stations::getAvailableForwardBuses() const {
 Queue<Bus*> Stations::getAvailableBackwardBuses() const {
     return availableBackwardBuses;
 }
+void Stations::unloadPassengers() {
+
+   
+    while (!recentBuses.isEmpty()) {
+
+        Bus* currentBus = recentBuses.peek();
+        if (typeid(*currentBus).name() == "MBus") {
+
+            currentBus = new MBus(int capacity, currentBus->get_current(), currentBus->get_destination());
+            currentBus->GetOff(Passenger * p);
+        }
+
+
+        else {
+
+            currentBus = new WBus(int capacity, currentBus->get_current(), currentBus->get_destination());
+
+        }
+
+
+        if (stationNumber == arrayList[stationNumber].size() - 1) {
+            currentBus->change_direction();
+        }
+        if (currentBus->get_direction() == "FWD") {
+
+            availableForwardBuses.enqueue(currentBus);
+
+
+        }
+        if (currentBus->get_direction() == "BWD") {
+            availableBackwardBuses.enqueue(currentBus);
+        }
+        recentBuses.dequeue();
+
+
+    }
+}
+
+void Stations::loadPassengers() {
+
+    while (!availableForwardBuses.isEmpty()) {
+
+        Bus* currentBus = availableForwardBuses.peek();
+
+        if ((typeid(*currentBus).name() == "MBus")) {
+
+            currentBus = new MBus(int capacity, currentBus->get_current(), currentBus->get_destination());
+
+
+            while (!forwardSP.isEmpty()) {
+
+
+                Passenger* currentPassenger = forwardSP.peek() ;
+                currentBus->GetOn(currentPassenger);
+                forwardSP.dequeue();
+
+
+
+
+            }
+
+
+            while (!forwardNP.isEmpty()) {
+
+
+                Passenger* currentPassenger = forwardNP.peek();
+                currentBus->GetOn(currentPassenger);
+                forwardNP.dequeue();
+
+
+            }
+
+
+
+        }
+
+        else {
+
+            currentBus = new WBus(int capacity, currentBus->get_current(), currentBus->get_destination());
+
+            while (!forwardWP.isEmpty()) {
+
+
+                Passenger* currentPassenger = forwardWP.peek();
+                currentBus->GetOn(currentPassenger);
+                forwardWP.dequeue();
+
+            }
+
+
+
+        }
+
+
+
+    }
+
+   /* ////////////////////////////////availableBackwardBuses///////////////////////////////////////////////
+    */
+    while (!availableBackwardBuses.isEmpty()) {
+
+        Bus* currentBus = availableBackwardBuses.peek();
+
+        if ((typeid(*currentBus).name() == "MBus")) {
+
+            currentBus = new MBus(int capacity, currentBus->get_current(), currentBus->get_destination());
+
+
+                while (!forwardSP.isEmpty()) {
+
+
+                    Passenger* currentPassenger = forwardSP.peek();
+                        currentBus->GetOn(currentPassenger);
+                        forwardSP.dequeue();
+
+
+
+
+                }
+
+
+            while (!forwardNP.isEmpty()) {
+
+
+                Passenger* currentPassenger = forwardNP.peek();
+                currentBus->GetOn(currentPassenger);
+                forwardNP.dequeue();
+
+
+            }
+
+
+
+        }
+
+        else {
+
+            currentBus = new WBus(int capacity, currentBus->get_current(), currentBus->get_destination());
+
+            while (!forwardWP.isEmpty()) {
+
+
+                Passenger* currentPassenger = forwardWP.peek();
+                currentBus->GetOn(currentPassenger);
+                forwardWP.dequeue();
+
+            }
+
+
+
+        }
+
+
+
+    }
+
+
+}
+
+
 
 void Stations::addPassengerToStation(Passenger* passenger) {
     if (passenger->getPassengerType() == "POD")
